91b41739a1ea510bf5d6fab2a8ab985930e06ce5
Ciremun <Ciremun@yandex.ru>
2021-07-20 00:56:32 +0300

    un arg

--- nobuild.c
+++ nobuild.c
@@ -1,12 +1,17 @@
 #define NOBUILD_IMPLEMENTATION
 #include "nobuild.h"
 
+#include "string.h"
+
 #define SOURCES "main.cpp"
+#define OUTPUT "opengl-playground"
 
 #ifdef _WIN32
 #define LIBS "-lglew32", "-lglfw3", "-lopengl32"
+#define RUN ".\\" OUTPUT ".exe"
 #else
 #define LIBS "-lGLEW", "-lglfw", "-lGL"
+#define RUN "./" OUTPUT
 #endif
 
 #define CXXFLAGS "-Wall", "-Wextra", "-pedantic", "-std=c++17", "-ggdb", "-O0"
@@ -16,6 +21,12 @@ int main(int argc, char **argv)
     GO_REBUILD_URSELF(argc, argv);
 
     char *cxx = getenv("cxx");
-    if (cxx == NULL) cxx = "g++";
+    if (cxx == NULL)
+        cxx = "g++";
     CMD(cxx, SOURCES, CXXFLAGS, "-oopengl-playground", LIBS);
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "run") == 0)
+            CMD(RUN);
+    }
 }
